7432111437933a1024fad9e6f5f81ae4fdc238a8
Laurent81000 <6279@holbertonstudents.com>
2023-03-30 04:11:27 -0700

    _printf function : c, s, and % - without switch - with _putchar in the code

--- 0-printf.c
+++ 0-printf.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include <limits.h>
-#include "_putchar"
+#include <unistd.h>
 #include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * _printf - prints output according to a format.
@@ -12,53 +23,53 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    const char *p;
-    va_list arg;
+	int count = 0;
+	const char *p;
+	va_list arg;
 
-    va_start(arg, format);
-    for (p = format; *p != '\0'; p++)
-    {
-        if (*p == '%')
-        {
-            p++;
-            if (*p == '\0')
-                return (-1);
-            else if (*p == 'c')
-            {
-                char c = va_arg(arg, int);
-                _putchar(c);
-                count++;
-            }
-            else if (*p == 's')
-            {
-                char *str = va_arg(arg, char*);
-                while (*str != '\0')
-                {
-                    _putchar(*str);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*p == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(*p);
-                count += 2;
-            }
-        }
-        else
-        {
-            _putchar(*p);
-            count++;
-        }
-    }
-    va_end(arg);
+	va_start(arg, format);
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '\0')
+				return (-1);
+			else if (*p == 'c')
+			{
+				char c = va_arg(arg, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*p == 's')
+			{
+				char *str = va_arg(arg, char*);
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*p == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(*p);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(*p);
+			count++;
+		}
+	}
+	va_end(arg);
 
-    return (count);
+	return (count);
 }
